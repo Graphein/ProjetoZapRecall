f010c4252cb2969d77a4c6d5650a2a12853e2e9e
Eduardo Leli <135680059+Graphein@users.noreply.github.com>
2025-03-02 13:59:02 -0300

    all

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: "./", 
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
 });
